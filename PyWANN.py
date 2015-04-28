b7709942bfedaf7919a9e132b4c4219366e6e178
Fabricio <firminodefaria@gmail.com>
2015-04-28 19:20:01 -0300

    arranging formatting

--- PyWANN.py
+++ PyWANN.py
@@ -38,7 +38,8 @@ class Memory:
         self.__data = {}
         self.__is_cummulative = is_cummulative
 
-        for i in range(2**num_bits):  # number of address positions 2 bits
+        # number of address positions 2 bits
+        for i in range(2**num_bits):
             self.__data[i] = 0
 
     def get_memory_data(self):
@@ -50,7 +51,8 @@ class Memory:
             raise Exception('address is not a valid list')
 
         if len(addr) > len(self.__data):
-            raise Exception('number of the bits of address are bigger than max size')
+            raise Exception('number of the bits of address are bigger \
+                             than max size')
 
         int_position = self.__list_to_int(addr)
 
@@ -70,24 +72,30 @@ class Memory:
 
 class Discriminator:
 
-    def __init__(self, retina_length, num_bits_addr=2, memories_values_cummulative=False, randomize_positions=True):
+    def __init__(self, retina_length, num_bits_addr=2,
+                 memories_values_cummulative=False,
+                 randomize_positions=True):
 
         self.__retina_length = retina_length
         self.__num_bits_addr = num_bits_addr
 
         if (retina_length % num_bits_addr) > 0:
-            raise Exception('incompatible number of memories for size of retina. Change mumber of addr bits')
+            raise Exception('incompatible number of memories for size of \
+                             retina. Change mumber of addr bits')
 
         num_memories = retina_length / num_bits_addr
 
         # creating list of memories
         self.__memories = {}
         for i in range(num_memories):
-            self.__memories[i] = Memory(num_bits_addr, memories_values_cummulative)
+            self.__memories[i] = Memory(num_bits_addr,
+                                        memories_values_cummulative)
 
         # mapping random positions (if randomize_positions is True)
         self.__memories_mapping = {}
-        position_list = range(retina_length)  # generating all possible positions
+
+        # generating all possible positions
+        position_list = range(retina_length)
 
         if randomize_positions:  # random positions to mapping aleatory
             rand.shuffle(position_list)
@@ -112,24 +120,38 @@ class Discriminator:
     def training(self, list_positive_retina):
         for retina in list_positive_retina:  # for each element of training
 
-            for memory_key in self.__memories_mapping:  # for each mapping position in retina, each position of n bits correspond an only one memory
+            # for each mapping position in retina, each position of n bits
+            # correspond an only one memory
+            for memory_key in self.__memories_mapping:
 
                 addr_list = []
-                position_list = self.__memories_mapping[memory_key]  # get the mapping positions (size is equal of number of address in the memory)
 
-                for position in position_list:  # for each position mapped get binary value (1 if position has value positive and 0 otherwise)
+                # get the mapping positions (size is equal of number of address
+                # in the memory)
+                position_list = self.__memories_mapping[memory_key]
+
+                # for each position mapped get binary value (1 if position has
+                # value positive and 0 otherwise)
+                for position in position_list:
                     if retina.get_data()[position] > 0:
                         addr_list.append(1)
                     else:
                         addr_list.append(0)
 
-                self.__memories[memory_key].add_value(addr_list, 1)  # add value 1 into the positon (defined by addr_list)
+                # add value 1 into the positon (defined by addr_list)
+                self.__memories[memory_key].add_value(addr_list, 1)
 
     def classifier(self, retina):
         result = 0
-        for memory_key in self.__memories_mapping:  # for each mapping position in retina, each position of n bits correspond an only one memory
+
+        # for each mapping position in retina, each position of n bits
+        # correspond an only one memory
+        for memory_key in self.__memories_mapping:
             addr_list = []
-            position_list = self.__memories_mapping[memory_key]  # get the mapping positions (size is equal of number of address in the memory)
+
+            # get the mapping positions (size is equal of number of address
+            # in the memory)
+            position_list = self.__memories_mapping[memory_key]
 
             for position in position_list:
                 if retina.get_data()[position] > 0:
