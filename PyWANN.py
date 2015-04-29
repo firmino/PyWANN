8cf6a70094cb3f5605ba1a08e4b78764657af7b0
Fabricio <firminodefaria@gmail.com>
2015-04-29 15:53:51 -0300

    change discriminator's creation memory method. Initializing wisard class

--- PyWANN.py
+++ PyWANN.py
@@ -48,7 +48,7 @@ class Memory:
     def add_value(self, addr, value=1):
 
         if type(addr) != list:
-            raise Exception('address is not a valid list')
+            raise Exception("address' type is not a list")
 
         if len(addr) > len(self.__data):
             raise Exception('number of the bits of address are bigger \
@@ -79,17 +79,14 @@ class Discriminator:
         self.__retina_length = retina_length
         self.__num_bits_addr = num_bits_addr
 
-        if (retina_length % num_bits_addr) > 0:
-            raise Exception('incompatible number of memories for size of \
-                             retina. Change mumber of addr bits')
-
-        num_memories = retina_length / num_bits_addr
+        num_memories = retina_length // num_bits_addr
 
         # creating list of memories
         self.__memories = {}
         for i in range(num_memories):
-            self.__memories[i] = Memory(num_bits_addr,
+            self.__memories[i] = Memory(self.__num_bits_addr,
                                         memories_values_cummulative)
+
 
         # mapping random positions (if randomize_positions is True)
         self.__memories_mapping = {}
@@ -97,13 +94,30 @@ class Discriminator:
         # generating all possible positions
         position_list = range(retina_length)
 
-        if randomize_positions:  # random positions to mapping aleatory
+        if randomize_positions: # random positions to mapping aleatory
             rand.shuffle(position_list)
 
         for i in range(num_memories):
             init = i * num_bits_addr
             end = init + num_bits_addr
             self.__memories_mapping[i] = position_list[init:end]
+
+
+        #if the retina's length is not a multiple of number of bits of
+        #addressing, is necessary create a smaller memory to map all positions.
+        #this memory will have the number of address bits equal to module of
+        #number of memories and number of bits of addressing
+        self.__num_bits_addr_final = retina_length % num_bits_addr
+        if self.__num_bits_addr_final > 0:
+
+            #adding in the last position of the list (position == num_memories)
+            self.__memories[num_memories] = Memory(self.__num_bits_addr_final,
+                                                   memories_values_cummulative)
+
+            #getting the last positions to mapping, how they are in the end of
+            #the randomized position list, we are using negative index of python
+            position_map = -1 * self.__num_bits_addr_final
+            self.__memories_mapping[num_memories] = position_list[position_map:]
 
     def get_memories(self):
         return self.__memories
@@ -154,10 +168,10 @@ class Discriminator:
             position_list = self.__memories_mapping[memory_key]
 
             for position in position_list:
-                if retina.get_data()[position] > 0:
                     addr_list.append(1)
                 else:
                     addr_list.append(0)
+                if retina.get_data()[position] > 0:
 
             result += self.__memories[memory_key].get_value(addr_list)
 
@@ -169,8 +183,46 @@ class Discriminator:
 
 class Wisard:
 
-    def __init__(self):
-        pass
+    def __init__(self, num_bits_addr=2, bleaching=None, is_cumulative=False ):
+        self.__num_bits_addr = num_bits_addr
+        self.__is_cumulative = is_cumulative
+        self.__bleaching_method = bleaching
+        self.__discriminators = {}
+
+
+    def add_discrimator(self, name, training_set):
+
+        #transform training_set(multidimensional matrix) to type Retina
+        retina_training_set = []
+        for element in training_set:
+            retina_training_set.append(Retina(element))
+
+        #getting the first retina to know the retina's size
+        retina_size = len(retina_training_set[0].get_data())
+
+        #creating discriminator
+        self.__discriminators[name] = Discriminator(retina_size,
+                                                    self.__num_bits_addr,
+                                                    self.__is_cumulative)
+
+        #training discriminator
+        self.__discriminators[name].training(retina_training_set)
+
+
+    def classifier(example):
+        result = {}
+
+        #become example a retina
+        r = Retina(example)
+
+        for class_name in self.__discriminators:
+            result[class_name] = self.__discriminators[class_name].classifier(r)
+
+        if self.__bleaching_method is not None:
+            result = self.__bleaching_method.apply(result)
+
+        return result
+
 
 class Bleaching:
 
--- tests/test_discriminator.py
+++ tests/test_discriminator.py
@@ -9,27 +9,29 @@ class TestDiscriminator(unittest.TestCase):
         d = Discriminator(9, 3)
         self.assertIsNotNone(d)
 
-    def test_invalid_number_of_bits_addrs_for_retina_size(self):
-        self.assertRaises(Exception, Discriminator, (8, 3))
-
-    def test_check_correct_number_of_memories(self):
-        d = Discriminator(12, 3)
+    def test_check_correct_number_of_memories_exact_mapping(self):
+        d = Discriminator(12, 3) # retina lenght is a multiple of number of address' bits
         num_mem = len(d.get_memories())
         self.assertEquals(num_mem, 4)
+
+    def test_check_correct_number_of_memories_not_exact_mapping(self):
+        d = Discriminator(14, 3) # retina lenght is not a multiple of number of address'
+                                 # bits
+        num_mem = len(d.get_memories())
+        self.assertEquals(num_mem, 5) # a addicional memory
 
     def test_random_mapping(self):
         d = Discriminator(12, 3)
 
         is_organized = True
-        list_organized_positions = range(12)
+        list_organized_positions = str.join("", map(str, range(12)) )
 
+        str_key = ""
         for memory_mapping in d._Discriminator__memories_mapping.values():
-            str_key_a = str.join("", map(str, list_organized_positions))
-            str_key_b = str.join("", map(str, memory_mapping))
+            str_key += str.join("", map(str, memory_mapping))
 
-            if str_key_b not in str_key_a:
-                is_organized = False
-                break
+        if str_key not in list_organized_positions:
+            is_organized = False
 
         self.assertFalse(is_organized)
 
@@ -37,17 +39,17 @@ class TestDiscriminator(unittest.TestCase):
         d = Discriminator(12, 3, memories_values_cummulative=False, randomize_positions=False)
 
         is_organized = True
-        list_organized_positions = range(12)
+        list_organized_positions = str.join("", map(str, range(12)) )
 
+        str_key = ""
         for memory_mapping in d._Discriminator__memories_mapping.values():
-            str_key_a = str.join("", map(str, list_organized_positions))
-            str_key_b = str.join("", map(str, memory_mapping))
+            str_key += str.join("", map(str, memory_mapping))
 
-            if str_key_b not in str_key_a:
-                is_organized = False
-                break
+        if str_key not in list_organized_positions:
+            is_organized = False
 
         self.assertTrue(is_organized)
+
 
     def test_training_no_cumulative(self):
 
@@ -64,7 +66,8 @@ class TestDiscriminator(unittest.TestCase):
         r1 = Retina(data1)
         r2 = Retina(data2)
 
-        d = Discriminator(9, 3)
+        d = Discriminator(9, 5) # will generate two memories (one with 5 bits and another
+                                # with 4 bits of addressing)
         d.training([r1, r2])
 
         # testing correct mapping
@@ -83,20 +86,39 @@ class TestDiscriminator(unittest.TestCase):
         # | mem-2  |  [4,2,5] | [0 1 0]      | [1 1 0]
         # | mem-3  |  [0,1,6] | [1 1 0]      | [1 1 0]
         #
-        # If we apply the mapping process for the expected result (that represents the same class)
-        # we have to find 1 in all memories positions
+        # If we apply the mapping process for the expected result (that represents the
+        # same class) we have to find 1 in all memories positions
 
         is_corrected_mapped = True
 
         for key in d.get_memories_mapping():
-            posi_0 = d.get_memories_mapping()[key][0]  # get first mapping position
-            posi_1 = d.get_memories_mapping()[key][1]  # get second mapping position
-            posi_2 = d.get_memories_mapping()[key][2]  # get third mapping position
 
-            addr = [expected_result[posi_0], expected_result[posi_1], expected_result[posi_2]]
-            if d.get_memory(key).get_value(addr) != 1:
-                is_corrected_mapped = False
-                break
+            if(len(d.get_memories_mapping()[key]) == 4):
+                posi_0 = d.get_memories_mapping()[key][0]  # get first mapping position
+                posi_1 = d.get_memories_mapping()[key][1]  # get second mapping position
+                posi_2 = d.get_memories_mapping()[key][2]  # get third mapping position
+                posi_3 = d.get_memories_mapping()[key][3]  # get fourth mapping position
+
+                addr = [expected_result[posi_0], expected_result[posi_1],
+                        expected_result[posi_2], expected_result[posi_3]]
+                if d.get_memory(key).get_value(addr) != 1:
+                    is_corrected_mapped = False
+                    break
+
+            if(len(d.get_memories_mapping()[key]) == 5):
+                posi_0 = d.get_memories_mapping()[key][0]  # get first mapping position
+                posi_1 = d.get_memories_mapping()[key][1]  # get second mapping position
+                posi_2 = d.get_memories_mapping()[key][2]  # get third mapping position
+                posi_3 = d.get_memories_mapping()[key][3]  # get fourth mapping position
+                posi_4 = d.get_memories_mapping()[key][4]  # get fifth mapping position
+
+                addr = [expected_result[posi_0], expected_result[posi_1],
+                        expected_result[posi_2], expected_result[posi_3],
+                        expected_result[posi_4]]
+
+                if d.get_memory(key).get_value(addr) != 1:
+                    is_corrected_mapped = False
+                    break
 
         self.assertTrue(is_corrected_mapped)
 
