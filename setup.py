ec3bc5bc2af5acd5850a3e59da56337beae48093
Fabricio <firminodefaria@gmail.com>
2015-06-09 19:18:25 -0300

    building egg package

---
+++ setup.py
@@ -0,0 +1,20 @@
+setup(
+    name ='PyWANN',
+    version='0.1',
+    description='Python Weightless Artificial Neural Networks',
+    author='Fabricio Firmino, Fabio Rangel',
+    author_email='firminodefaria@gmail.com, kojiro@gmail.com',
+    url='http://ppgi.ufrj.br/',
+    packages=['nome_do_pacote'])
+    long_description="""\
+        Python Weightless Artificial Neural Networks lib
+    """,
+    classifiers=[
+        "Weightless neural networks", "machine learning"
+    ],
+    
+    license='Apache License',
+    install_requires=[
+        'setuptools',
+    ],
+ )
