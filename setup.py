700dc7b7df67a4517daa062ee4b8fb5c9b28c8ee
Fabricio <firminodefaria@gmail.com>
2015-06-09 19:30:38 -0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,14 @@ setup(
     author='Fabricio Firmino, Fabio Rangel',
     author_email='firminodefaria@gmail.com, kojiro@gmail.com',
     url='http://ppgi.ufrj.br/',
-    packages=['nome_do_pacote'])
-    long_description="""\
-        Python Weightless Artificial Neural Networks lib
-    """,
-    classifiers=[
-        "Weightless neural networks", "machine learning"
-    ],
+    packages=['nome_do_pacote'],
+
+    long_description=" Python Weightless Artificial Neural Networks lib",
+
+    classifiers=["Weightless neural networks", "machine learning"],
 
     license='Apache License',
+    
     install_requires=[
         'setuptools',
     ]
